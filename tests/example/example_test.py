16bf1be8ca022f561b503ac1fc591a7d23f548a6
rguo123 <richardg7890@gmail.com>
2018-09-19 16:25:16 -0400

    pytest, requirements txt, and ci

---
+++ tests/example/example_test.py
@@ -0,0 +1,13 @@
+''' 
+    This is an example test to demo how tests will work with 
+    CI within our repositoryself.
+'''
+#TODO: delete this test and directory
+
+import pytest
+
+
+#this is an example test
+def test_example():
+    assert (1 == 1)
+    
