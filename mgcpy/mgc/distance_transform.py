a4f42a755ec385971ecc865df8d6e86f4b2f1ce6
tpsatish95 <tpsatish95@gmail.com>
2018-09-27 17:32:47 -0400

    Add transform_distance_matrix function

--- mgcpy/mgc/distance_transform.py
+++ mgcpy/mgc/distance_transform.py
@@ -1,3 +1,7 @@
+"""
+    MGCPY Distance Transform
+"""
+
 import numpy as np
 import scipy.stats
 
@@ -37,14 +41,18 @@ def center_distance_matrix(distance_matrix, base_global_correlation="mgc", is_ra
     :type distance_matrix: 2D numpy.array
 
     :param base_global_correlation: specifies which global correlation to build up-on,
-                                    including 'mgc','dcor','mantel', and 'rank'
-    :type base_global_correlation: string
+                                    including 'mgc','dcor','mantel', and 'rank'.
+                                    Defaults to mgc.
+    :type base_global_correlation: str
 
     :param is_ranked: specifies whether ranking within a column is computed or not
-    :type is_ranked: boolean
+                      Defaults to True.
+    :type is_ranked: bool
 
-    :return: dict(centered ``distance_matrix``, column-wise ranked ``distance_matrix``)
-    :rtype: dictionary
+    :return: A ``dict`` with the following keys:
+    :rtype: dict
+        - :centered_distance_matrix: a [n*n] centered distance matrix
+        - :ranked_distance_matrix: a [n*n] column-ranked distance matrix
     """
     n = distance_matrix.shape[0]
     ranked_distance_matrix = np.zeros(distance_matrix.shape)
@@ -56,7 +64,8 @@ def center_distance_matrix(distance_matrix, base_global_correlation="mgc", is_ra
         distance_matrix = ranked_distance_matrix
 
     # 'mgc' distance transform (col-wise mean) - default
-    expected_distance_matrix = np.repeat(((distance_matrix.mean(axis=0) * n) / (n-1)), n).reshape(-1, n).T
+    expected_distance_matrix = np.repeat(
+        ((distance_matrix.mean(axis=0) * n) / (n-1)), n).reshape(-1, n).T
 
     # unbiased version of dcor distance transform (col-wise mean + row-wise mean - mean)
     if base_global_correlation == "dcor":
@@ -76,3 +85,57 @@ def center_distance_matrix(distance_matrix, base_global_correlation="mgc", is_ra
 
     return {"centered_distance_matrix": centered_distance_matrix,
             "ranked_distance_matrix": ranked_distance_matrix}
+
+
+def transform_distance_matrix(distance_matrix_A, distance_matrix_B, base_global_correlation="mgc", is_ranked=True):
+    """
+    Transforms the distance matrices appropriately, with column-wise ranking if needed.
+
+    :param distance_matrix_A: first symmetric distance matrix, [n*n]
+    :type distance_matrix: 2D numpy.array
+
+    :param distance_matrix_B: second symmetric distance matrix, [n*n]
+    :type distance_matrix: 2D numpy.array
+
+    :param base_global_correlation: specifies which global correlation to build up-on,
+                                    including 'mgc','dcor','mantel', and 'rank'.
+                                    Defaults to mgc.
+    :type base_global_correlation: str
+
+    :param is_ranked: specifies whether ranking within a column is computed or not,
+                      if, base_global_correlation = "rank", then ranking is performed
+                      regardless of the value if is_ranked. Defaults to True.
+    :type is_ranked: bool
+
+    :return: A ``dict`` with the following keys:
+    :rtype: dict
+        - :centered_distance_matrix_A: a [n*n] centered distance matrix of A
+        - :centered_distance_matrix_B: a [n*n] centered distance matrix of B
+        - :ranked_distance_matrix_A: a [n*n] column-ranked distance matrix of A
+        - :ranked_distance_matrix_B: a [n*n] column-ranked distance matrix of B
+
+    **Example:**
+    >>> import numpy as np
+    >>> from scipy.spatial import distance_matrix
+
+    >>> X = np.array([[2, 1, 100], [4, 2, 10], [8, 3, 10]])
+    >>> Y = np.array([[30, 20, 10], [5, 10, 20], [8, 16, 32]])
+    >>> X_distance_matrix = distance_matrix(X, X)
+    >>> Y_distance_matrix = distance_matrix(Y, Y)
+    >>> transformed_distance_matrix_X_Y = transform_distance_matrix(X_distance_matrix, Y_distance_matrix)
+    """
+
+    if base_global_correlation == "rank":
+        is_ranked = True
+
+    transformed_distance_matrix_A = center_distance_matrix(
+        distance_matrix_A, base_global_correlation, is_ranked)
+    transformed_distance_matrix_B = center_distance_matrix(
+        distance_matrix_B, base_global_correlation, is_ranked)
+
+    transformed_distance_matrix = {"centered_distance_matrix_A": transformed_distance_matrix_A["centered_distance_matrix"],
+                                   "centered_distance_matrix_B": transformed_distance_matrix_B["centered_distance_matrix"],
+                                   "ranked_distance_matrix_A": transformed_distance_matrix_A["ranked_distance_matrix"],
+                                   "ranked_distance_matrix_B": transformed_distance_matrix_B["ranked_distance_matrix"]}
+
+    return transformed_distance_matrix
--- mgcpy/mgc/tests/distance_transform_test.py
+++ mgcpy/mgc/tests/distance_transform_test.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from scipy.spatial import distance_matrix
 
-from mgcpy.mgc.distance_transform import rank_distance_matrix, center_distance_matrix
+from mgcpy.mgc.distance_transform import rank_distance_matrix, center_distance_matrix, transform_distance_matrix
 
 
 def test_rank_distance_matrix():
@@ -58,3 +58,33 @@ def test_center_distance_matrix_mantel():
 
     assert np.allclose(X_centered_distance_matrix_mantel,
                        center_distance_matrix(X_distance_matrix, "mantel")["centered_distance_matrix"])
+
+
+def test_transform_distance_matrix():
+    X = np.array([[2, 1, 100], [4, 2, 10], [8, 3, 10]])
+    Y = np.array([[30, 20, 10], [5, 10, 20], [8, 16, 32]])
+    X_distance_matrix = distance_matrix(X, X)
+    Y_distance_matrix = distance_matrix(Y, Y)
+    X_centered_distance_matrix = np.array([[0.00000000, 42.95233, 43.04942],
+                                           [-0.09708753, 0.00000, -43.04942],
+                                           [0.09708753, -42.95233, 0.00000]])
+    X_ranked_distance_matrix = np.array([[1, 3, 3],
+                                         [2, 1, 2],
+                                         [3, 2, 1]])
+    Y_centered_distance_matrix = np.array([[0.000000, 7.487543, 8.810524],
+                                           [-1.322981, 0.000000, -8.810524],
+                                           [1.322981, -7.487543, 0.000000]])
+    Y_ranked_distance_matrix = np.array([[1, 3, 3],
+                                         [2, 1, 2],
+                                         [3, 2, 1]])
+    transformed_distance_matrix_X_Y = transform_distance_matrix(
+        X_distance_matrix, Y_distance_matrix)
+
+    assert np.allclose(X_centered_distance_matrix,
+                       transformed_distance_matrix_X_Y["centered_distance_matrix_A"])
+    assert np.allclose(Y_centered_distance_matrix,
+                       transformed_distance_matrix_X_Y["centered_distance_matrix_B"])
+    assert np.allclose(X_ranked_distance_matrix,
+                       transformed_distance_matrix_X_Y["ranked_distance_matrix_A"])
+    assert np.allclose(Y_ranked_distance_matrix,
+                       transformed_distance_matrix_X_Y["ranked_distance_matrix_B"])
