1e6625e8330316cc542ff4261f4497f89a7a9940
junhaobearxiong <jxiong3@jhu.edu>
2018-09-29 16:34:44 -0400

    changed file folder structures, changed test file name

--- mgcpy/dcorr/tests/dcorr_test.py
+++ mgcpy/dcorr/tests/dcorr_test.py
@@ -1,9 +1,9 @@
 import pytest
 import numpy as np
-from mgcpy.mgc.dcorr import dcorr
+from mgcpy.dcorr.dcorr import dcorr
 
 
-def dcorr_test():
+def test_dcorr():
     # small simulated example of exponential dependency
     X = np.array([1.1728, 2.4941, 2.4101, 0.1814, 1.1978, 1.5806, 1.2504, 1.9706, 1.8839, 0.8760])[:, np.newaxis]
     Y = np.array([3.2311, 12.1113, 11.1350, 1.1989, 3.3127, 4.8580, 3.4917, 7.1748, 6.5792, 2.4012])[:, np.newaxis]
