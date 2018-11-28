add83f47ff781502a5c6ad0a597b1fb69c2a6afb
tpsatish95 <tpsatish95@gmail.com>
2018-11-27 20:48:10 -0500

    Refactor independence_tests abstract

--- mgcpy/independence_tests/abstract_class.py
+++ mgcpy/independence_tests/abstract_class.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from scipy.spatial.distance import pdist, squareform
 
 
 class IndependenceTest(ABC):
@@ -8,19 +9,16 @@ class IndependenceTest(ABC):
     Specifies the generic interface that must be implemented by
     all the independence tests in the mgcpy package.
 
-    :param data_matrix_X: a [n*p] data matrix, a square matrix with n samples in p dimensions
-    :type data_matrix_X: 2D `numpy.array`
-
-    :param data_matrix_Y: a [n*q] data matrix, a square matrix with n samples in q dimensions
-    :type data_matrix_Y: 2D `numpy.array`
-
     :param compute_distance_matrix: a function to compute the pairwise distance matrix, given a data matrix
     :type compute_distance_matrix: FunctionType or callable()
     """
 
-    def __init__(self, data_matrix_X, data_matrix_Y, compute_distance_matrix):
-        self.data_matrix_X = data_matrix_X
-        self.data_matrix_Y = data_matrix_Y
+    def __init__(self, compute_distance_matrix=None):
+        self.test_statistic = None
+        self.p_value = None
+        if not compute_distance_matrix:
+            def EUCLIDEAN_DISTANCE(x): return squareform(pdist(x, metric='euclidean'))
+            compute_distance_matrix = EUCLIDEAN_DISTANCE
         self.compute_distance_matrix = compute_distance_matrix
         super().__init__()
 
@@ -48,5 +46,27 @@ class IndependenceTest(ABC):
             - :independence_test_metadata: (optional) metadata other than the test_statistic,
                                            that the independence tests computes in the process
         :rtype: float, dict
+        """
+        pass
+
+    @abstractmethod
+    def p_value(self, data_matrix_X, data_matrix_Y, replication_factor=1000):
+        """
+        Tests independence between two datasets using MGC and permutation test.
+
+        :param data_matrix_X: a [n*p] data matrix, a square matrix with n samples in p dimensions
+        :type data_matrix_X: 2D `numpy.array`
+
+        :param data_matrix_Y: a [n*q] data matrix, a square matrix with n samples in q dimensions
+        :type data_matrix_Y: 2D `numpy.array`
+
+        :param replication_factor: specifies the number of replications to use for
+                                   the permutation test. Defaults to 1000.
+        :type replication_factor: int
+
+        :return: returns a list of two items, that contains:
+            - :p_value: P-value of MGC
+            - :metadata: (optional) a ``dict`` of metadata other than the p_value,
+                         that the independence tests computes in the process
         """
         pass
