df2bac97d5227e29dd010faf0863bf914ce407e5
junhaobearxiong <jxiong3@jhu.edu>
2018-12-06 11:27:07 -0500

    added unit test for dcorr, change mantel to not use absolute value, reran mantel power, fixed hhg bug

--- mgcpy/independence_tests/dcorr.py
+++ mgcpy/independence_tests/dcorr.py
@@ -68,10 +68,13 @@ class DCorr(IndependenceTest):
         self.test_statistic_metadata_ = {'variance_X': variance_X, 'variance_Y': variance_Y, 'covariance': covariance}
 
         # use absolute value for mantel coefficients
+        '''
         if self.which_test == 'mantel':
             self.test_statistic_ = np.abs(correlation)
         else:
             self.test_statistic_ = correlation
+        '''
+        self.test_statistic_ = correlation
 
         return (self.test_statistic_, self.test_statistic_metadata_)
 
