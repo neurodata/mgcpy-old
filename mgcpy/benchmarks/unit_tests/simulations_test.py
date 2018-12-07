5c94dbffd02b2fab52da5fda15ac2708d9e77fcb
Sambit Panda <spanda3@jhu.edu>
2018-12-07 07:28:24 -0500

    Fixed issues with hhg

--- mgcpy/benchmarks/unit_tests/simulations_test.py
+++ mgcpy/benchmarks/unit_tests/simulations_test.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from mgcpy.benchmarks import simulations as sims
-import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
@@ -11,24 +11,24 @@ def test_simulations():
     independent = True
 
     np.random.seed(0)
-    fig1 = plt.figure(figsize = (50, 80))
+    fig1 = plt.figure(figsize=(50, 80))
     plt.axis('off')
-    fig2 = plt.figure(figsize = (50, 80))
+    fig2 = plt.figure(figsize=(50, 80))
 
     # Linear Simulation
     returns_low_dim = sims.linear_sim(num_samps, num_dim1)
     returns_high_dim = sims.linear_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.linear_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 1)
     ax1.scatter(x1, y1)
     ax1.set_title('Linear', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.linear_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 1, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 1, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Linear', fontweight='bold')
     ax2.axis('off')
@@ -38,15 +38,15 @@ def test_simulations():
     returns_high_dim = sims.exp_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.exp_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 2)
     ax1.scatter(x1, y1)
     ax1.set_title('Exponential', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.exp_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 2, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 2, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Exponential', fontweight='bold')
     ax2.axis('off')
@@ -56,15 +56,15 @@ def test_simulations():
     returns_high_dim = sims.cub_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-     
+
     x1, y1 = sims.cub_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 3)
     ax1.scatter(x1, y1)
     ax1.set_title('Cubic', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.cub_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 3, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 3, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Cubic', fontweight='bold')
     ax2.axis('off')
@@ -74,15 +74,15 @@ def test_simulations():
     returns_high_dim = sims.joint_sim(num_samps, num_dim2)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.joint_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 4)
     ax1.scatter(x1, y1)
     ax1.set_title('Joint Normal', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.joint_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 4, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 4, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Joint Normal', fontweight='bold')
     ax2.axis('off')
@@ -92,15 +92,15 @@ def test_simulations():
     returns_high_dim = sims.step_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.step_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 5)
     ax1.scatter(x1, y1)
     ax1.set_title('Step', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.step_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 5, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 5, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Step', fontweight='bold')
     ax2.axis('off')
@@ -110,15 +110,15 @@ def test_simulations():
     returns_high_dim = sims.quad_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.quad_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 6)
     ax1.scatter(x1, y1)
     ax1.set_title('Quadratic', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.quad_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 6, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 6, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Quadratic', fontweight='bold')
     ax2.axis('off')
@@ -128,15 +128,15 @@ def test_simulations():
     returns_high_dim = sims.w_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.w_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 7)
     ax1.scatter(x1, y1)
     ax1.set_title('W-Shaped', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.w_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 7, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 7, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('W-Shaped', fontweight='bold')
     ax2.axis('off')
@@ -146,15 +146,15 @@ def test_simulations():
     returns_high_dim = sims.spiral_sim(num_samps, num_dim2)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.spiral_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 8)
     ax1.scatter(x1, y1)
     ax1.set_title('Spiral', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.spiral_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 8, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 8, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Spiral', fontweight='bold')
     ax2.axis('off')
@@ -162,15 +162,15 @@ def test_simulations():
     # Uncorrelated Bernoulli Simulation
     returns = sims.ubern_sim(num_samps, num_dim2)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.ubern_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 9)
     ax1.scatter(x1, y1)
     ax1.set_title('Uncorrelated Bernoulli', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.ubern_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 9, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 9, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Uncorrelated Bernoulli', fontweight='bold')
     ax2.axis('off')
@@ -180,16 +180,16 @@ def test_simulations():
     returns_high_dim = sims.log_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.log_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 10)
     ax1.scatter(x1, y1)
     ax1.set_title('Logarithmic', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.log_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 10, projection = '3d');
-    ax2.scatter(x2[:, 0], x2[:, 1], y2)
+    ax2 = fig2.add_subplot(4, 5, 10, projection='3d')
+    ax2.scatter(x2[:, 0], x2[:, 1], y2[:, 0]*y2[:, 1])
     ax2.set_title('Logarithmic', fontweight='bold')
     ax2.axis('off')
 
@@ -198,15 +198,15 @@ def test_simulations():
     returns_high_dim = sims.root_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.root_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 11)
     ax1.scatter(x1, y1)
     ax1.set_title('Fourth Root', fontweight='bold')
     ax1.axis('off')
-    
+
     x2, y2 = sims.root_sim(num_samps, 2, noise=0)
-    ax2 = fig2.add_subplot(4, 5, 11, projection = '3d');
+    ax2 = fig2.add_subplot(4, 5, 11, projection='3d')
     ax2.scatter(x2[:, 0], x2[:, 1], y2)
     ax2.set_title('Fourth Root', fontweight='bold')
     ax2.axis('off')
@@ -216,7 +216,7 @@ def test_simulations():
     returns_high_dim = sims.sin_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.sin_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 12)
     ax1.scatter(x1, y1)
@@ -229,7 +229,7 @@ def test_simulations():
         num_samps, num_dim2, period=16*np.pi, indep=independent)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.sin_sim(num_samps, 1, noise=0, period=16*np.pi)
     ax1 = fig1.add_subplot(4, 5, 13)
     ax1.scatter(x1, y1)
@@ -239,7 +239,7 @@ def test_simulations():
     # Square Simulation
     returns = sims.square_sim(num_samps, num_dim2, indep=independent)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.square_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 14)
     ax1.scatter(x1, y1)
@@ -251,7 +251,7 @@ def test_simulations():
     returns_high_dim = sims.two_parab_sim(num_samps, num_dim2)
     assert np.all(returns_low_dim[0].shape == (num_samps, num_dim1))
     assert np.all(returns_high_dim[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.two_parab_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 15)
     ax1.scatter(x1, y1)
@@ -261,7 +261,7 @@ def test_simulations():
     # Circle Simulation
     returns = sims.circle_sim(num_samps, num_dim2)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.circle_sim(num_samps, 1, noise=0)
     ax1 = fig1.add_subplot(4, 5, 16)
     ax1.scatter(x1, y1)
@@ -271,7 +271,7 @@ def test_simulations():
     # Ellipse Simulation
     returns = sims.circle_sim(num_samps, num_dim2, radius=5)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.circle_sim(num_samps, 1, noise=0, radius=5)
     ax1 = fig1.add_subplot(4, 5, 17)
     ax1.scatter(x1, y1)
@@ -282,7 +282,7 @@ def test_simulations():
     returns = sims.square_sim(
         num_samps, num_dim2, period=-np.pi/4, indep=independent)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.square_sim(num_samps, 1, noise=0, period=-np.pi/4)
     ax1 = fig1.add_subplot(4, 5, 18)
     ax1.scatter(x1, y1)
@@ -292,7 +292,7 @@ def test_simulations():
     # Multiplicative Noise Simulation
     returns = sims.multi_noise_sim(num_samps, num_dim2)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.multi_noise_sim(num_samps, 1)
     ax1 = fig1.add_subplot(4, 5, 19)
     ax1.scatter(x1, y1)
@@ -302,11 +302,12 @@ def test_simulations():
     # Multimodal Independence Simulation
     returns = sims.multi_indep_sim(num_samps, num_dim2)
     assert np.all(returns[0].shape == (num_samps, num_dim2))
-    
+
     x1, y1 = sims.multi_indep_sim(num_samps, 1)
     ax1 = fig1.add_subplot(4, 5, 20)
     ax1.scatter(x1, y1)
     ax1.set_title('Multimodal Independence', fontweight='bold')
     ax1.axis('off')
+
 
 test_simulations()
