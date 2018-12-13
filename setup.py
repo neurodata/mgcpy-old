2bef2a6a9afdee45dc9fe025fe429e885e73bced
tpsatish95 <tpsatish95@gmail.com>
2018-12-13 14:13:28 -0500

    Fix setup.py imports

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@
 
 import os
 import sys
-from subprocess import call, check_output
-from sys import platform
-
-import numpy
-from Cython.Build import cythonize
-from setuptools import find_packages, setup
+from setuptools import setup, find_packages
 from setuptools.command.install import install
+from subprocess import check_output, call
+from Cython.Build import cythonize
+from sys import platform
+import numpy
 
 PACKAGE_NAME = 'mgcpy'
 DESCRIPTION = 'A set of tools in Python for multiscale graph correlation and other statistical tests'
