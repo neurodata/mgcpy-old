b66cb4174506237ad43399ff253296a7a9d6165c
Sambit Panda <spanda3@jhu.edu>
2019-03-06 10:22:36 -0500

    Fixed compute_distance_matrix to correctly compute distance

--- mgcpy/independence_tests/utils/compute_distance_matrix.py
+++ mgcpy/independence_tests/utils/compute_distance_matrix.py
@@ -23,8 +23,8 @@ def compute_distance(matrix_X, matrix_Y, _compute_distance):
     """
     # use the matrix shape and diagonal elements to determine if the given data is a distance matrix or not
     if matrix_X.shape[0] != matrix_X.shape[1] or sum(matrix_X.diagonal()**2) > 0:
-        distance_matrix_X = _compute_distance(matrix_X)
+        matrix_X = _compute_distance(matrix_X)
     if matrix_Y.shape[0] != matrix_Y.shape[1] or sum(matrix_Y.diagonal()**2) > 0:
-        distance_matrix_Y = _compute_distance(matrix_Y)
+        matrix_Y = _compute_distance(matrix_Y)
 
-    return distance_matrix_X, distance_matrix_Y
+    return matrix_X, matrix_Y
