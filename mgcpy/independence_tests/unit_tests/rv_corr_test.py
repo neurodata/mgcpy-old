851e9b18d8127458ddd9f4d0f61a8f825c8447f5
Sambit Panda <spanda3@jhu.edu>
2019-05-23 17:00:50 -0400

    fox unit test for rvcorr

--- mgcpy/independence_tests/unit_tests/rv_corr_test.py
+++ mgcpy/independence_tests/unit_tests/rv_corr_test.py
@@ -7,9 +7,9 @@ from mgcpy.independence_tests.rv_corr import RVCorr
 def test_local_corr():
     # Against a randomly defined data set
     X = np.array([1.1728, 2.4941, 2.4101, 0.1814, 1.1978, 1.5806, 1.2504,
-                  1.9706, 1.8839, 0.8760])[:, np.newaxis]
+                  1.9706, 1.8839, 0.8760], dtype='float')[:, np.newaxis]
     Y = np.array([3.2311, 12.1113, 11.1350, 1.1989, 3.3127, 4.8580, 3.4917,
-                  7.1748, 6.5792, 2.4012])[:, np.newaxis]
+                  7.1748, 6.5792, 2.4012], dtype='float')[:, np.newaxis]
     rvcorr = RVCorr(None)
     rvcorr2 = RVCorr(None, 'pearson')
     rvcorr3 = RVCorr(None, 'cca')
@@ -38,3 +38,6 @@ def test_local_corr():
     assert np.round(test_stat1, decimals=2) == 0.24
     assert np.round(test_stat2, decimals=2) == 0.49
     assert np.round(test_stat3, decimals=2) == 0.24
+
+
+test_local_corr()
