2d306fd638e86ce26fc1c0d1e10b5c6457097884
j1c <jaewonc78@gmail.com>
2019-10-30 11:02:31 -0400

    Add Gretton 2014 nonlinear dep simulation

--- mgcpy/benchmarks/ts_benchmarks.py
+++ mgcpy/benchmarks/ts_benchmarks.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 
 # Time series simulation classes.
 
+
 class TimeSeriesProcess(ABC):
     """
     TimeSeriesProcess abstract class
@@ -32,12 +33,13 @@ class TimeSeriesProcess(ABC):
 
         pass
 
+
 class IndependentAR1(TimeSeriesProcess):
     def __init__(self):
-        self.name = 'Independent AR(1)'
-        self.filename = 'indep_ar1'
+        self.name = "Independent AR(1)"
+        self.filename = "indep_ar1"
 
-    def simulate(self, n, phi = 0.5, sigma2 = 1.0):
+    def simulate(self, n, phi=0.5, sigma2=1.0):
         """
         Method to simulate observations of the process.
 
@@ -69,18 +71,19 @@ class IndependentAR1(TimeSeriesProcess):
         Y[0] = etas[0]
 
         # AR(1) process.
-        for t in range(1,n):
-            X[t] = phi*X[t-1] + epsilons[t]
-            Y[t] = phi*Y[t-1] + etas[t]
+        for t in range(1, n):
+            X[t] = phi * X[t - 1] + epsilons[t]
+            Y[t] = phi * Y[t - 1] + etas[t]
 
         return X, Y
 
+
 class CorrelatedAR1(TimeSeriesProcess):
     def __init__(self):
-        self.name = 'Correlated AR(1)'
-        self.filename = 'corr_ar1'
+        self.name = "Correlated AR(1)"
+        self.filename = "corr_ar1"
 
-    def simulate(self, n, phi = 0.5, sigma2 = 1.0):
+    def simulate(self, n, phi=0.5, sigma2=1.0):
         """
         Method to simulate observations of the process.
 
@@ -111,18 +114,19 @@ class CorrelatedAR1(TimeSeriesProcess):
         X[0] = epsilons[0]
         Y[0] = etas[0]
 
-        for t in range(1,n):
-            X[t] = phi*Y[t-1] + epsilons[t]
-            Y[t] = phi*X[t-1] + etas[t]
+        for t in range(1, n):
+            X[t] = phi * Y[t - 1] + epsilons[t]
+            Y[t] = phi * X[t - 1] + etas[t]
 
         return X, Y
 
+
 class NonlinearLag1(TimeSeriesProcess):
     def __init__(self):
-        self.name = 'Nonlinearly Related Lag 1'
-        self.filename = 'nonlin_lag1'
+        self.name = "Nonlinearly Related Lag 1"
+        self.filename = "nonlin_lag1"
 
-    def simulate(self, n, sigma2 = 1.0):
+    def simulate(self, n, sigma2=1.0):
         """
         Method to simulate observations of the process.
 
@@ -151,7 +155,68 @@ class NonlinearLag1(TimeSeriesProcess):
         Y[0] = etas[0]
 
         for t in range(1, n):
-            X[t] = epsilons[t]*Y[t-1]
+            X[t] = epsilons[t] * Y[t - 1]
             Y[t] = etas[t]
 
         return X, Y
+
+
+class NonlinearDependence(TimeSeriesProcess):
+    """
+    Nonlinear dependence setting proposed by Gretton et al 2014.
+    Parameter defaults are set to replicate results from paper.
+    """
+
+    def __init__(self):
+        self.name = "Nonlinear Dependence"
+        self.filename = "nonlin_dependence"
+
+    def simulate(self, n, extinction_rate, radius=1, alpha=0.2):
+        """
+        Method to simulate observations of the process.
+
+        :param n: sample_size
+        :type n: integer
+
+        :param extinction_rate: Rate of extinction \in [0, 1]
+        :type extinction_rate: float
+
+        :param radius: radius (default: 1)
+        :type radius: float
+
+        :param alpha: autoregressive component (default: 0.2)
+        :type alpha: float
+
+        :return: returns a list of two items, that contains:
+
+            - :X: a ``[n*1]`` data matrix, a matrix with n samples
+            - :Y: a ``[n*1]`` data matrix, a matrix with n samples
+        :rtype: list
+        """
+        if (extinction_rate < 0) or (extinction_rate > 1):
+            msg = "extinction_rate must be between 0 and 1, inclusive."
+            raise ValueError(msg)
+
+        # Innovations.
+        eta, epsilon = self._sample_innovations(extinction_rate, radius)
+
+        X = np.zeros(n)
+        Y = np.zeros(n)
+        X[0] = epsilon
+        Y[0] = eta
+
+        for t in range(1, n):
+            eta, epsilon = self._sample_innovations(extinction_rate, radius)
+            X[t] = alpha * X[t - 1] + epsilon
+            Y[t] = alpha * Y[t - 1] + eta
+
+        return X, Y
+
+    def _sample_innovations(self, extinction_rate, radius):
+        while True:
+            eta = np.random.normal(0, 1)
+            epsilon = np.random.normal(0, 1)
+            d = np.random.uniform()
+
+            if (eta ** 2 + epsilon ** 2 > radius ** 2) or (d > extinction_rate):
+                return eta, epsilon
